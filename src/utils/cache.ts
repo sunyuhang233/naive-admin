dfe4ec1b8674c31b27344f75f7c8a754ebd6cd3e
ahang <2283582431@qq.com>
2025-04-12 17:30:28 +0800

    fix: 修复storage

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -5,18 +5,24 @@ interface StorageData<T> {
   expire: number | null
 }
 
-class CreateLocalStorage<T extends Storage.Local> {
-  setItem(key: keyof T, value: T[keyof T], expire: number = 60 * 60 * 24 * 7) {
+class Storage<T> {
+  private storage: globalThis.Storage
+
+  constructor(storage: globalThis.Storage) {
+    this.storage = storage
+  }
+
+  setItem(key: keyof T, value: T[keyof T], expire: number | null = null) {
     const storageData: StorageData<T[keyof T]> = {
       value,
-      expire: new Date().getTime() + expire * 1000,
+      expire: expire ? new Date().getTime() + expire * 1000 : null,
     }
     const json = JSON.stringify(storageData)
-    window.localStorage.setItem(`${STORAGE_PREFIX}_${String(key)}`, json)
+    this.storage.setItem(`${STORAGE_PREFIX}_${String(key)}`, json)
   }
 
   getItem(key: keyof T) {
-    const json = window.localStorage.getItem(`${STORAGE_PREFIX}_${String(key)}`)
+    const json = this.storage.getItem(`${STORAGE_PREFIX}_${String(key)}`)
     if (!json) {
       return null
     }
@@ -34,44 +40,13 @@ class CreateLocalStorage<T extends Storage.Local> {
   }
 
   remove(key: keyof T) {
-    window.localStorage.removeItem(`${STORAGE_PREFIX}_${String(key)}`)
+    this.storage.removeItem(`${STORAGE_PREFIX}_${String(key)}`)
   }
 
   clear() {
-    window.localStorage.clear()
+    this.storage.clear()
   }
 }
 
-class CreateSessionStorage<T extends Storage.Session> {
-  setItem(key: keyof T, value: T[keyof T]) {
-    const json = JSON.stringify(value)
-    window.sessionStorage.setItem(`${STORAGE_PREFIX}_${String(key)}`, json)
-  }
-
-  getItem(key: keyof T) {
-    const json = window.sessionStorage.getItem(`${STORAGE_PREFIX}_${String(key)}`)
-    if (!json) {
-      return null
-    }
-
-    const storageData: T[keyof T] | null = JSON.parse(json)
-
-    if (storageData) {
-      return storageData
-    }
-
-    return null
-  }
-
-  remove(key: keyof T) {
-    window.sessionStorage.removeItem(`${STORAGE_PREFIX}_${String(key)}`)
-  }
-
-  clear() {
-    window.sessionStorage.clear()
-  }
-}
-
-export const local = new CreateLocalStorage<Storage.Local>()
-
-export const session = new CreateSessionStorage<Storage.Session>()
+export const local = new Storage<Storage.Local>(localStorage)
+export const session = new Storage<Storage.Session>(sessionStorage)
