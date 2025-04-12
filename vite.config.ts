c78718438fd15611b7ed7bb9b5860ed84ab2b8bf
ahang <2283582431@qq.com>
2025-04-12 13:01:31 +0800

    feat: 安装开发依赖

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,25 @@
 import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
-import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
     UnoCSS(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
+      dts: 'src/types/auto-imports.d.ts',
+      dirs: ['./src/composables'],
+      vueTemplate: true,
+    }),
+    Components({
+      dts: 'src/types/components.d.ts',
+      extensions: ['vue'],
+      include: [/\.vue$/, /\.vue\?vue/],
+    }),
 
   ],
   resolve: {
