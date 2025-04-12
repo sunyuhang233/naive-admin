15acbca46ee2a8f5563470231f383ae383a41092
ahang <2283582431@qq.com>
2025-04-12 11:02:49 +0800

    feat: 初始化项目

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import path from 'node:path'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '~/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
+  server: {
+    port: 8848,
+    open: true
+  }
+})
