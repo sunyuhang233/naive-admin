6eeab0780e7feee25b62b412461e2197462aec16
ahang <2283582431@qq.com>
2025-04-19 12:11:39 +0800

    feat: 上下布局

---
+++ src/layouts/components/menu/index.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const inverted = ref(false)
+
+const menuStore = useMenuStore()
+</script>
+
+<template>
+  <n-menu
+    :inverted="inverted"
+    :collapsed-width="64"
+    :collapsed-icon-size="22"
+    :options="menuStore.menus"
+    :value="menuStore.activeMenu"
+  />
+</template>
