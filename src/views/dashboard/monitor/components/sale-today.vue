1d3152d7265be217339a41daa7e2b54ac6afac92
ahang <2283582431@qq.com>
2025-04-20 18:22:54 +0800

    feat: 监控页

---
+++ src/views/dashboard/monitor/components/sale-today.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <n-card class="h-260px" title="今日销售">
+    <template #header-extra>
+      <n-button quaternary type="primary">
+        <template #icon>
+          <Icon icon="ri:download-2-fill" :size="20" />
+        </template>
+        下载
+      </n-button>
+    </template>
+    <div class="flex gap-4 items-center justify-between">
+      <n-card>
+        <n-thing>
+          <n-el class="flex flex-col gap-1">
+            <n-icon-wrapper :size="42" color="var(--success-color)" :border-radius="999">
+              <Icon :size="24" icon="icon-park-outline:chart-line" />
+            </n-icon-wrapper>
+            <n-statistic label="总销售额">
+              <n-number-animation show-separator :from="0" :to="12039" />
+            </n-statistic>
+            <p class="text-gray-500">
+              24.5% 较上月
+            </p>
+          </n-el>
+        </n-thing>
+      </n-card>
+      <n-card>
+        <n-thing>
+          <n-el class="flex flex-col gap-1">
+            <n-icon-wrapper :size="42" color="var(--info-color)" :border-radius="999">
+              <Icon :size="24" icon="icon-park-outline:order" />
+            </n-icon-wrapper>
+            <n-statistic label="总订单数">
+              <n-number-animation show-separator :from="0" :to="12039" />
+            </n-statistic>
+            <p class="text-gray-500">
+              24.5% 较上月
+            </p>
+          </n-el>
+        </n-thing>
+      </n-card><n-card>
+        <n-thing>
+          <n-el class="flex flex-col gap-1">
+            <n-icon-wrapper :size="42" color="var(--error-color)" :border-radius="999">
+              <Icon :size="24" icon="icon-park-outline:shopping-bag" />
+            </n-icon-wrapper>
+            <n-statistic label="已售产品">
+              <n-number-animation show-separator :from="0" :to="12039" />
+            </n-statistic>
+            <p class="text-gray-500">
+              24.5% 较上月
+            </p>
+          </n-el>
+        </n-thing>
+      </n-card><n-card>
+        <n-thing>
+          <n-el class="flex flex-col gap-1">
+            <n-icon-wrapper :size="42" color="var(--warning-color)" :border-radius="999">
+              <Icon :size="24" icon="icon-park-outline:people" />
+            </n-icon-wrapper>
+            <n-statistic label="新客户">
+              <n-number-animation show-separator :from="0" :to="12039" />
+            </n-statistic>
+            <p class="text-gray-500">
+              24.5% 较上月
+            </p>
+          </n-el>
+        </n-thing>
+      </n-card>
+    </div>
+  </n-card>
+</template>
--- src/views/dashboard/monitor/index.vue
+++ src/views/dashboard/monitor/index.vue
@@ -1,5 +1,33 @@
+<script setup lang="ts">
+import CustomerSatisfaction from './components/customer-satisfaction.vue'
+import RevenueAnalysis from './components/revenue-analysis.vue'
+import SaleToday from './components/sale-today.vue'
+import SalesDistribution from './components/sales-distribution.vue'
+import VisitorAnalysis from './components/visitor-analysis.vue'
+</script>
+
 <template>
-  <div>
-    <h1>监控页</h1>
+  <div class="flex flex-col gap-6">
+    <n-grid responsive="screen" item-responsive x-gap="16" y-gap="16">
+      <n-gi span="24 m:14 l:15">
+        <SaleToday />
+      </n-gi>
+      <n-gi span="24 m:10 l:9">
+        <VisitorAnalysis />
+      </n-gi>
+    </n-grid>
+    <n-grid responsive="screen" item-responsive x-gap="16" y-gap="16">
+      <n-gi span="24 m:10 l:10">
+        <RevenueAnalysis />
+      </n-gi>
+      <n-gi span="24 m:7 l:7">
+        <CustomerSatisfaction />
+      </n-gi>
+      <n-gi span="24 m:7 l:7">
+        <SalesDistribution />
+      </n-gi>
+    </n-grid>
   </div>
 </template>
+
+
