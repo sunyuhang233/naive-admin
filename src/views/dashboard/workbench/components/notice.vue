cadc3ad30d26c770354ea73ca524633694a4c70a
ahang <2283582431@qq.com>
2025-04-20 11:33:13 +0800

    feat: 工作台

---
+++ src/views/dashboard/workbench/components/notice.vue
@@ -0,0 +1,56 @@
+<template>
+  <n-card title="公告">
+    <template #header-extra>
+      <n-button
+        type="primary"
+        quaternary
+      >
+        更多
+      </n-button>
+    </template>
+    <n-list>
+      <n-list-item>
+        <template #prefix>
+          <n-tag
+            :bordered="false"
+            type="info"
+            size="small"
+          >
+            通知
+          </n-tag>
+        </template>
+        <n-button text>
+          漂洋过海上大专
+        </n-button>
+      </n-list-item>
+      <n-list-item>
+        <template #prefix>
+          <n-tag
+            :bordered="false"
+            type="success"
+            size="small"
+          >
+            消息
+          </n-tag>
+        </template>
+        <n-button text>
+          你在玩很新的东西
+        </n-button>
+      </n-list-item>
+      <n-list-item>
+        <template #prefix>
+          <n-tag
+            :bordered="false"
+            type="warning"
+            size="small"
+          >
+            活动
+          </n-tag>
+        </template>
+        <n-button text>
+          上岸第一剑，先斩意中人
+        </n-button>
+      </n-list-item>
+    </n-list>
+  </n-card>
+</template>
--- src/views/dashboard/workbench/index.vue
+++ src/views/dashboard/workbench/index.vue
@@ -1,9 +1,25 @@
+<script setup lang="ts">
+import Chat from './components/chat.vue'
+import CountBox from './components/count-box.vue'
+import Dynamic from './components/dynamic.vue'
+import Notice from './components/notice.vue'
+import TimeLine from './components/time-line.vue'
+import TotalBox from './components/total-box.vue'
+</script>
+
 <template>
   <div class="flex gap-3">
     <div class="flex flex-1 flex-col gap-3">
-      <div class="bg-purple-200 h-400px" />
-      <div class="p-3 bg-gray-200 dark:bg-red-500" />
+      <div class="h-400px">
+        <Chat />
+      </div>
+      <TotalBox />
+      <Dynamic />
     </div>
-    <div class="bg-green-200 h-300px w-400px" />
+    <div class="flex flex-col gap-3 w-400px">
+      <Notice />
+      <CountBox />
+      <TimeLine />
+    </div>
   </div>
 </template>
